ffcac0dcb9b6b22b864bb7f7b1625400c08d4925
Guilherme Puida <guilhermepmoreira00@gmail.com>
2022-12-11 20:52:18 -0300

    Simplified main run script

--- run.sh
+++ run.sh
@@ -4,46 +4,8 @@ set -eu
 
 echo "Advent of Languages"
 
-echo "Day 01 - Javascript"
-./day01-javascript/run.sh
-echo
-
-echo "Day 02 - Python"
-./day02-python/run.sh
-echo
-
-echo "Day 03 - Ruby"
-./day03-ruby/run.sh
-echo
-
-echo "Day 04 - Lua"
-./day04-lua/run.sh
-echo
-
-echo "Day 05 - Crystal"
-./day05-crystal/run.sh
-echo
-
-echo "Day 06 - C"
-./day06-c/run.sh
-echo
-
-echo "Day 07 - Go"
-./day07-go/run.sh
-echo
-
-echo "Day 08 - Cpp"
-./day08-cpp/run.sh
-echo
-
-echo "Day 09 - Clojure"
-./day09-clojure/run.sh
-echo
-
-echo "Day 10 - Nim"
-./day10-nim/run.sh
-echo
-
-echo "Day 11 - Janet"
-./day11-janet/run.sh
-echo
+for i in *day*; do
+  echo "$i"
+  "$i/run.sh"
+  echo
+done
