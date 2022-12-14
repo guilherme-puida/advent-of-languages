bc35e6ee511053bf6a18760a72d91c3137e22e89
Guilherme Puida <guilhermepmoreira00@gmail.com>
2022-12-13 23:11:07 -0300

    Remove comments from day01 solution

--- day01-javascript/solution.js
+++ day01-javascript/solution.js
@@ -6,5 +6,5 @@ const elves = input.split("\n\n").map((elf) =>
   elf.split("\n").map(Number).reduce((a, b) => a + b)
 ).sort((a, b) => b - a);
 
-console.log("Part 1:", elves[0]); // 71502
-console.log("Part 2:", elves.slice(0, 3).reduce((a, b) => a + b, 0)); // 208191
+console.log("Part 1:", elves[0]);
+console.log("Part 2:", elves.slice(0, 3).reduce((a, b) => a + b, 0));
